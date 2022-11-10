7faf83279c3aacf569d7da20e1fe0d7c4bb6593f
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-11-10 11:58:28 +0300

    u know allready x2

--- source/ModchartState.hx
+++ source/ModchartState.hx
@@ -1,7 +1,7 @@
 // this file is for modchart things, this is to declutter playstate.hx
 
 // Lua
-#if cpp
+#if !web
 import flixel.tweens.FlxEase;
 import openfl.filters.ShaderFilter;
 import flixel.tweens.FlxTween;
