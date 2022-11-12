7ff08ff892df1194de2f5c78594f5b98c3062917
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-11-12 22:22:22 +0300

    Create DaCutscene.hx

---
+++ source/DaCutscene.hx
@@ -0,0 +1,21 @@
+#if android
+import extension.videoview.VideoView;
+#end
+import flixel.FlxBasic;
+
+class DaCutscene extends FlxBasic {
+	public var finishCallback:Void->Void = null;
+
+	public function new(name:String) {
+		super();
+
+	        #if android
+                VideoView.playVideo('file:///sdcard/.stevolderidk/assets/videos/armorsteve.webm');
+                VideoView.onCompletion = function(){
+		        if (finishCallback != null){
+			        finishCallback();
+		        }
+                }
+		#end
+	}
+}
