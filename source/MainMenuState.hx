1cf0936e7ab3fecc7bd3189a9150b30a2ff46b67
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-11-13 16:17:12 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -52,6 +52,9 @@ class MainMenuState extends MusicBeatState
 
 	override function create()
 	{
+                Paths.clearUnusedMemory();
+                Paths.clearStoredMemory();
+
 		#if windows
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("In the Menus", null);
