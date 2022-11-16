f3d7771abbcae16174f13f31ae8a14c9eadce620
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-11-17 01:01:51 +0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -134,17 +134,10 @@ class Paths
 		return if (library == "preload" || library == "default") getPreloadPath(file); else getLibraryPathForce(file, library);
 	}
 
-        #if web
 	inline static function getLibraryPathForce(file:String, library:String)
-	{
-		return 'assets/$library/$file';
-	}
-        #else
-        inline static function getLibraryPathForce(file:String, library:String)
 	{
 		return '$library:assets/$library/$file';
 	}
-        #end
 
 	inline static function getPreloadPath(file:String)
 	{
