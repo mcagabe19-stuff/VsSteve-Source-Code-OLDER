c10fb31359e0597bc2d13a3856226c638a686a3f
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-01 19:34:46 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -244,7 +244,7 @@ class PlayState extends MusicBeatState
 	private var floatvalue:Float = 0;
 	private var runvalue:Float = 0;
 
-        public static var pausebutton:String = '';
+        public static var pausebutton = '';
 
 	override public function create()
 	{
