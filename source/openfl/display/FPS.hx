f95a28fdfb4c63e12dfc876cc92517f68dde1989
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-23 20:56:42 +0300

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -78,11 +78,6 @@ class FPS extends TextField
 		var currentCount = times.length;
 		currentFPS = Math.round((currentCount + cacheCount) / 2);
 
-		if (currentFPS > Main.framerate)
-		{
-			currentFPS = Main.framerate;
-		}
-
 		if (currentCount != cacheCount /*&& visible*/)
 		{
                 text = "FPS: " + currentFPS;
