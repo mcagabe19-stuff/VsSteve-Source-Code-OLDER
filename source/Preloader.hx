b103a2916188d7885367bccb4fccd7611f0675df
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-03-26 14:51:13 +0300

    Create Preloader.hx

---
+++ source/Preloader.hx
@@ -0,0 +1,111 @@
+package source;
+ 
+import flixel.util.FlxTimer;
+import flixel.util.FlxColor;
+#if sys
+import sys.net.Address;
+#end
+import openfl.text.TextFormat;
+import Paths;
+import openfl.text.TextField;
+import openfl.text.Font;
+import flixel.system.FlxBasePreloader;
+import openfl.display.Sprite;
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.display.Sprite;
+import flash.Lib;
+import flixel.FlxG;
+import flixel.math.FlxMath;
+import flash.display.BlendMode;
+ 
+@:bitmap("assets/preload/images/stevetitle.png") class Bg extends BitmapData {}
+//@:bitmap("assets/preload/images/gfDanceTitleNoAnim.png") class AlexAndStev extends BitmapData {}
+@:bitmap("assets/preload/images/logoBumpin.png") class LogoShit extends BitmapData {}
+@:font("assets/fonts/vcr.ttf") class CustomFont extends Font {}
+ 
+class Preloader extends FlxBasePreloader
+{
+	public static var max:Float = 10;
+
+    public function new(MinDisplayTime:Float=11) 
+    {
+        new FlxTimer().start(9.5, function(time:FlxTimer) {
+            FlxG.camera.fade(FlxColor.BLACK, 1, false);
+        });
+        max = MinDisplayTime;
+        super(MinDisplayTime);
+
+    }
+    var loadtxtProgress:Bitmap;
+    var loadtxtBg:Bitmap;
+    var text:TextField;
+    var logo:Sprite;
+    var bg:Sprite;
+    override function create():Void 
+    {
+        trace('loading from source/Preloader');
+        this._width = Lib.current.stage.stageWidth;
+        this._height = Lib.current.stage.stageHeight;
+         
+        var ratio:Float = this._width / 5000;
+        
+        bg = new Sprite();
+        bg.addChild(new Bitmap(new Bg (-100,-65)));
+        addChild(bg);
+        
+        logo = new Sprite();
+        logo.addChild(new Bitmap(new LogoShit (1,1)));
+        logo.x += 50;
+        logo.y += 50;
+        
+        //theshit = new Sprite();
+        //theshit.addChild(new Bitmap(new AlexAndStev (1,1)));
+        //theshit.x -= -100
+        //theshit.y +=
+        //addChild(theshit);
+        addChild(logo);
+         
+        loadtxtBg = new Bitmap(new BitmapData(1, 26, false, 0xFF000000));
+        loadtxtBg.alpha = 0.8;
+        loadtxtBg.scaleX = 1280;
+        addChild(loadtxtBg);
+
+        loadtxtProgress = new Bitmap(new BitmapData(1, 26, false, 0xffffff));
+        loadtxtProgress.alpha = 0.3;
+	addChild(loadtxtProgress);
+
+        Font.registerFont(CustomFont);
+        text = new TextField();
+        text.defaultTextFormat = new TextFormat("vcr", 24, 0xffffff, false, false ,false , null, null, "right");
+        text.embedFonts = true;
+        text.text = "Loading...";
+        text.width = 189;
+        text.y = 685;
+        text.x = -60;
+        addChild(text);
+
+        text.y += 2;
+        loadtxtBg.scaleY = text.height + 5;
+        loadtxtBg.y = text.y;
+        loadtxtBg.x = text.x + 60;
+        loadtxtProgress.scaleY = text.height + 5;
+        loadtxtProgress.y = text.y;
+        loadtxtProgress.x = text.x + 60;
+        super.create();
+    }
+    override function update(Percent:Float):Void
+        {
+            loadtxtProgress.scaleX = Percent * (_width - 0.5);
+        }
+        override function destroy():Void
+            {
+                bg = null;
+                //theshit = null;
+                logo = null;
+                loadtxtProgress = null;
+                text = null;
+                loadtxtBg = null;
+                super.destroy();
+            }
+}
